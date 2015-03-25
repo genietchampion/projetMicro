d5b1aa3dfd667adf0b16c86c61dff5e4e692a228
Corentin Normand <bichonche@gmail.com>
2015-03-25 10:55:05 +0100

    Create mainFonctions.h
    Toutes les fonctions utiles pour Melaine.

---
+++ mainFonctions.h
@@ -0,0 +1,53 @@
+/*********************************************************
+* variables globales
+**********************/
+// variables globales messages
+typedef struct message
+{
+	char dir;
+	int valeur;
+}typeMessage;
+
+typeMessage tabMessage[8];
+
+//variables globales de deplacement
+int speed, distance, angle;
+/**************************
+**	Deplacements				**
+**************************/
+//avancer
+void forward(int speed, int distance);
+//tourner
+void rotation(int angle);
+//arreter
+void stop();
+
+/*******************
+** Affichage  ecran LCD
+********************/
+
+//initialiser l'ecran
+void initScreen();
+//affichage
+void printScreen(char * message, char posX, char posY);
+//nettoyer
+void clearScreen();
+
+/*********************
+** emission IR 
+*********************/
+
+void emitMessage(typeMessage * tableau);
+
+/****************
+** reception IR
+****************/
+
+//TODO Etienne, à toi de remplir cette partie
+
+/***************
+** SON
+****************/
+
+
+
